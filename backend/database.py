c1bc54ec34a78973776b00c022e0fe1fa2be65e6
jadz200 <jadzarzour3@gmail.com>
2022-11-13 23:22:11 +0200

    new database

--- backend/database.py
+++ backend/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://felp_db_user:kVBBmTugRJKjLLJBVNbplPBLjy7qyeej@dpg-cdoknkmn6mpuqrtihkfg-a.frankfurt-postgres.render.com/felp_db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
